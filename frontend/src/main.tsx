68a2ed81b74261fa7bc460ce776c856337ada45a
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-08-14 09:01:21 -0400

    style: load rich editor presentation

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./organization.css";
+import "./rich-editor.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
